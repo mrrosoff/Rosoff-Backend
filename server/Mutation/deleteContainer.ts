ad1153f3ec21fdffed82a1d2d0c426b073de4254
Max Rosoff <me@maxrosoff.com>
2021-06-17 12:10:04 -0600

    New Docker driver lolz

--- server/Mutation/deleteContainer.ts
+++ server/Mutation/deleteContainer.ts
@@ -9,11 +9,12 @@ const deleteContainer = async (_: any, args: Args, context: Context, info: any) 
 	await checkIsLoggedIn(context);
 	await checkIsContainerOwner(context, args.id);
 	const container = context.docker.getContainer(args.id);
+	const containerInfo = container.inspect();
 	container.delete({ force: true });
 	context.db
 		.collection("Users")
 		.updateOne({ _id: context.userId }, { $pull: { containers: container.id } });
-	return container.inspect();
+	return containerInfo;
 };
 
 export default deleteContainer;
